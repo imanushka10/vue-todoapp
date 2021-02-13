8febe27c8e78ade48127251bfee783f16e13e25f
Anushka Singh <anusingh1883656@gmail.com>
2021-02-13 14:44:27 +0530

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="todo-item" v-bind:class="{ 'is-complete': todo.completed }">
+    <p><input type="checkbox" v-on:change="markComplete" />{{ todo.title }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      console.log("Clicked");
+    },
+  },
+};
+</script>
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-radius: 1px solid dotted;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+</style>
